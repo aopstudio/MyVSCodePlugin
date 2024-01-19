7c9a1441f56a28c6cc27453ee4d726f41519bb8a
Cen Zhedong <cenzhedong2@126.com>
2024-01-19 16:17:54 +0800

    在状态栏显示时钟

--- src/extension.ts
+++ src/extension.ts
@@ -7,26 +7,72 @@ import moment from 'moment';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+// export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "helloworld" is now active!');
+// 	// Use the console to output diagnostic information (console.log) and errors (console.error)
+// 	// This line of code will only be executed once when your extension is activated
+// 	console.log('Congratulations, your extension "helloworld" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('helloworld.helloWorld', async () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		const ip = await ipily();
-		const query = new IP2Region();
-		const ipAddress: {country:string, province:string, city:string} | null = query.search(ip);
-		const {country=''} = ipAddress || {};
-		vscode.window.showInformationMessage(`当前时间为：${moment().format('YYYY-MM-DD HH:mm:ss')}，当前位置为${country}`);
-	});
+// 	// The command has been defined in the package.json file
+// 	// Now provide the implementation of the command with registerCommand
+// 	// The commandId parameter must match the command field in package.json
+// 	let disposable = vscode.commands.registerCommand('helloworld.helloWorld', async () => {
+// 		// The code you place here will be executed every time your command is executed
+// 		// Display a message box to the user
+// 		const ip = await ipily();
+// 		const query = new IP2Region();
+// 		const ipAddress: {country:string, province:string, city:string} | null = query.search(ip);
+// 		const {country=''} = ipAddress || {};
+// 		vscode.window.showInformationMessage(`当前时间为：${moment().format('YYYY-MM-DD HH:mm:ss')}，当前位置为${country}`);
+// 	});
 
-	context.subscriptions.push(disposable);
+// 	context.subscriptions.push(disposable);
+// }
+
+let myStatusBarItem: vscode.StatusBarItem;
+
+export function activate({ subscriptions }: vscode.ExtensionContext) {
+
+	// register a command that is invoked when the status bar
+	// item is selected
+	const myCommandId = 'sample.showSelectionCount';
+
+	subscriptions.push(vscode.commands.registerCommand(myCommandId, async () => {
+		const time = getTime();
+		const country = await getLocation();
+		vscode.window.showInformationMessage(`当前时间为：${time}，当前位置为${country}`);
+	}));
+
+	// create a new status bar item that we can now manage
+	myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+	myStatusBarItem.command = myCommandId;
+	subscriptions.push(myStatusBarItem);
+
+	// register some listener that make sure the status bar 
+	// item always up-to-date
+	subscriptions.push(vscode.window.onDidChangeActiveTextEditor(updateStatusBarItem));
+	subscriptions.push(vscode.window.onDidChangeTextEditorSelection(updateStatusBarItem));
+
+	// update status bar item once at start
+	setInterval(updateStatusBarItem, 1000);
+	updateStatusBarItem();
+}
+
+function updateStatusBarItem(): void {
+	myStatusBarItem.text = getTime();
+	myStatusBarItem.show();
+}
+
+function getTime(): string {
+	return moment().format('YYYY-MM-DD HH:mm:ss');
+}
+
+async function getLocation(): Promise<string> {
+	const ip = await ipily();
+	const query = new IP2Region();
+	const ipAddress: {country:string, province:string, city:string} | null = query.search(ip);
+	const {country='',province='',city=''} = ipAddress || {};
+	return city||province||country;
 }
 
 // This method is called when your extension is deactivated
